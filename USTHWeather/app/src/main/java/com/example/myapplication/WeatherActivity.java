7340664398ec420a03b24a912af4d20416ba87fe
Đỗ Bá Hoàng Minh <hoangminhdoba@gmail.com>
2024-09-20 15:36:18 +0700

    lab 8

--- USTHWeather/app/src/main/java/com/example/myapplication/WeatherActivity.java
+++ USTHWeather/app/src/main/java/com/example/myapplication/WeatherActivity.java
@@ -1,23 +1,54 @@
-package vn.edu.usth.weather;
-
 import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import com.google.android.material.tabs.TabLayout;
 
 public class WeatherActivity extends AppCompatActivity {
+
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-        ViewPager viewPager = findViewById(R.id.viewPager);
+        // Initialize ViewPager and TabLayout
+        viewPager = findViewById(R.id.viewPager);
+        tabLayout = findViewById(R.id.tabLayout);
 
-        WeatherPagerAdapter adapter = new WeatherPagerAdapter(
-                getSupportFragmentManager(),
-                FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        // Set up ViewPager with a FragmentPagerAdapter
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager(), BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+            @Override
+            public Fragment getItem(int position) {
+                // Return new instances of WeatherAndForecastFragment
+                return new WeatherAndForecastFragment();
+            }
 
-        viewPager.setAdapter(adapter);
+            @Override
+            public int getCount() {
+                return 3; // 3 fragments for 3 tabs
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                switch (position) {
+                    case 0:
+                        return "Tab 1";
+                    case 1:
+                        return "Tab 2";
+                    case 2:
+                        return "Tab 3";
+                    default:
+                        return null;
+                }
+            }
+        });
+
+        // Connect TabLayout with ViewPager
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
