b9186c87389e4dfe1ad8bfb2c5545d77f32628d4
Đỗ Bá Hoàng Minh <hoangminhdoba@gmail.com>
2024-09-20 15:30:06 +0700

    Lab 7

--- USTHWeather/app/src/main/java/com/example/myapplication/WeatherActivity.java
+++ USTHWeather/app/src/main/java/com/example/myapplication/WeatherActivity.java
@@ -1,61 +1,23 @@
-package com.example.myapplication;
+package vn.edu.usth.weather;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.FragmentTransaction;
-
+import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.FragmentPagerAdapter;
 
 public class WeatherActivity extends AppCompatActivity {
-    private static final String TAG = "WeatherActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-        ForecastFragment firstFragment = new ForecastFragment();
+        ViewPager viewPager = findViewById(R.id.viewPager);
 
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.container, firstFragment);
-        transaction.commit();
-        Log.i(TAG, "onCreate: Activity created");
+        WeatherPagerAdapter adapter = new WeatherPagerAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        viewPager.setAdapter(adapter);
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i(TAG, "onStart: Activity started");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i(TAG, "onResume: Activity resumed");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i(TAG, "onPause: Activity paused");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i(TAG, "onStop: Activity stopped");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(TAG, "onDestroy: Activity destroyed");
-    }
-
 }
---
+++ USTHWeather/app/src/main/java/com/example/myapplication/WeatherPagerAdapter.java
@@ -0,0 +1,24 @@
+package vn.edu.usth.weather;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class WeatherPagerAdapter extends FragmentPagerAdapter {
+
+    public WeatherPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return new WeatherAndForecastFragment();
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
